be2e93eb57838090380f0e469fdbdc244a3c13cb
krunal-ar-panchal <155724372+krunal-ar-panchal@users.noreply.github.com>
2024-06-18 15:08:39 +0530

    blog app

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ import userRouter from "./Routes/userRoute.js";
 import postRoutes from "./Routes/postRoute.js";
 import commentRoutes from "./Routes/commetRoute.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 const app = express();
 config();
-
 
 const PORT = process.env.PORT;
 
@@ -32,15 +36,11 @@ app.use("/api/user", userRouter);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-if(process.env.MODE_ENV === "production"){
-  const dirPath = path.resolve();
-  app.use(express.static("client/dist"));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(dirPath,"client","dist","index.html"))
-  })
-}
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 
 app.listen(PORT, () => {
   console.log(`${PORT}`);
